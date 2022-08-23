f0e2c3de225bc96bf71b7273febcc675535c3bdd
undiscloser <kilicoguzhan@gmail.com>
2022-08-23 14:30:31 +0300

    lesson 04

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+
+dotenv.config();
+
+// connection to the DB
+conn();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
+
+// ejs template engine
+
+app.set("view engine", "ejs");
+
+// static files middleware
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI 2");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
