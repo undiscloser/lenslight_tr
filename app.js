7288373ce92288741ee1bd07a5eee103c7ef2cee
undiscloser <kilicoguzhan@gmail.com>
2022-08-26 12:05:42 +0300

    lesson 06

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
 import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(express.static("public"));
 //routes
 
 app.use("/", pageRoute);
+app.use("/photos", photoRoute);
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
