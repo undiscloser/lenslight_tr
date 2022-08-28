c18b0a1095f9e21d4a30766bf2dc564ea32540f7
undiscloser <kilicoguzhan@gmail.com>
2022-08-28 15:59:06 +0300

    lesson 11

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,5 @@
 const getIndexPage = (req, res) => {
+  console.log("request user:::", req.user);
   res.render("index", {
     link: "index",
   });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,17 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json({
-      succeded: true,
+      succeeded: true,
       user,
     });
   } catch (error) {
     res.status(500).json({
-      succeded: false,
+      succeeded: false,
       error,
     });
   }
@@ -19,35 +20,41 @@ const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    console.log("req.body", req.body);
-
     const user = await User.findOne({ username });
 
     let same = false;
 
     if (user) {
       same = await bcrypt.compare(password, user.password);
-      console.log("same", same);
     } else {
       return res.status(401).json({
-        succeded: false,
+        succeeded: false,
         error: "There is no such user",
       });
     }
     if (same) {
-      res.status(200).send("You are logged in");
+      res.status(200).json({
+        user,
+        token: createToken(user._id),
+      });
     } else {
       res.status(401).json({
-        succeded: false,
+        succeeded: false,
         error: "Password are not match",
       });
     }
   } catch (error) {
     res.status(500).json({
-      succeded: false,
+      succeeded: false,
       error,
     });
   }
+};
+
+const createToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 };
 
 export { createUser, loginUser };
